3e2f1f7dae9c98d24113d135b0c40e16c97df9da
RANK <81941006+AtSignRANK@users.noreply.github.com>
2021-10-17 17:27:53 +0900

    [ = ] pug -> html

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import express from "express";
 
 const app = express();
 
-app.set("view engine", "pug");
+app.set("view engine", "html");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
